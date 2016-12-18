e93d02ce15d85e4913068401909edb29a15c36e8
Edward Marshall <ruckafella@gmail.com>
2016-12-18 13:46:29 +0000

    Adding internal metrics for the exporter

---
+++ measure/measure.go
@@ -0,0 +1,35 @@
+package measure
+
+import (
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+//
+var (
+	// Create a summary to track elapsed time of our key functions
+	FunctionDurations = prometheus.NewSummaryVec(
+		prometheus.SummaryOpts{
+			Name:       "function_durations_seconds",
+			Help:       "Function timings for Rancher Exporter",
+			Objectives: map[float64]float64{0.5: 0.05, 0.9: 0.01, 0.99: 0.001},
+		}, []string{"service"})
+
+	// Create a counter to track total executions of the functions
+	FunctionCountTotal = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "function_count_totals",
+			Help: "total count of function calls",
+		}, []string{"pkg", "fnc"})
+
+	start = time.Now()
+)
+
+// init registers the prometheus metrics for the measurement of the exporter itsself.
+func Init() {
+
+	prometheus.MustRegister(FunctionDurations)
+	prometheus.MustRegister(FunctionCountTotal)
+
+}
